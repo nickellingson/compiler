f660d26f562e97f2be70d3a392e9419f137a048e
Nicholas Ellingson <nickellingson8@gmail.com>
2024-05-18 17:37:56 -0500

    file io

--- cmd/run/run.go
+++ cmd/run/run.go
@@ -3,11 +3,20 @@ package main
 import (
 	"fmt"
 
+	"github.com/nickellingson/compiler/internal/fileInputOutput"
 	"github.com/nickellingson/compiler/internal/lexicalAnalysis"
 )
 
 
 func main () {
 	fmt.Println("start of main")
+
+	// testFile, _ := filepath.Abs("../../precompiled/test1.txt")
+	testFile := "../../precompiled/test1.txt"
+
+	// Read file
+	fileInputOutput.ReadFile(testFile)
+
+	// Lexical Analysis
 	lexicalAnalysis.Tokenize()
 }
